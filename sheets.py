2cae98eac15916f0fdf053899ff0c478b0b0795c
W10 Pro SS <cotrends.design@gmail.com>
2026-04-01 13:54:24 -0500

    cambios

--- sheets.py
+++ sheets.py
@@ -5,7 +5,7 @@ import json
 
 SHEET_ID = "1a2R35CCW2ANqxs539Jqm7iEXPjcB4bxPhA6Tvr6_6kw"
 BASE_URL = "https://docs.google.com/spreadsheets/d/" + SHEET_ID + "/gviz/tq?tqx=out:csv"
-APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxXANplf9G9RwOOQQYPPLG7PoEnvEUKbxjvSBO8AXZtBhq8Fbfv-F_RZ2rOItFL9g/exec"
+APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx38ITmjTGc86t2xSIo-7Vm0_SECsODz9GPEp2s5NH6EiUva39R0j9rNPOUURFPxQ5_Cw/exec"
 
 async def get_catalogo() -> str:
     url = BASE_URL + "&sheet=Catalogo"
